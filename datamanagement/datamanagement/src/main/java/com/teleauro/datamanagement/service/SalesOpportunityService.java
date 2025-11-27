fb9e193c302e2448bf4d742ce41d0abf075d50e3
Ubuntu <azureuser@TeleAuroServer.1gxheg3kommuhpigdkrsree2ce.fx.internal.cloudapp.net>
2025-11-27 15:15:12 +0000

    Backend DB Integration

---
+++ datamanagement/datamanagement/src/main/java/com/teleauro/datamanagement/service/SalesOpportunityService.java
@@ -0,0 +1,45 @@
+
+package com.teleauro.datamanagement.service;
+
+import com.teleauro.datamanagement.model.SalesOpportunity;
+import com.teleauro.datamanagement.repository.SalesOpportunityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SalesOpportunityService {
+
+    @Autowired
+    private SalesOpportunityRepository repository;
+
+    public SalesOpportunity create(SalesOpportunity opportunity) {
+        return repository.save(opportunity);
+    }
+
+    public List<SalesOpportunity> readAll() {
+        return repository.findAll();
+    }
+
+    public Optional<SalesOpportunity> readById(Long uprn) {
+        return repository.findById(uprn);
+    }
+
+    public SalesOpportunity update(Long uprn, SalesOpportunity updated) {
+        if (repository.existsById(uprn)) {
+            updated.setUprn(uprn);
+            return repository.save(updated);
+        }
+        return null;
+    }
+
+    public boolean delete(Long uprn) {
+        if (repository.existsById(uprn)) {
+            repository.deleteById(uprn);
+            return true;
+        }
+        return false;
+    }
+}
